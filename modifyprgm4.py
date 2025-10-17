b5533a807e52b86e4f79f39ef42dc54d01ff83b7
ashwinipatil1281@gmail.com <ashwinipatil1281-dev>
2025-10-17 21:14:48 +0530

    save

---
+++ modifyprgm4.py
@@ -0,0 +1,9 @@
+a=(input("enter first number:"));
+b=(input("enter second number:"));
+print("addition:",a+b);
+print("subtraction:",a-b);
+print("multiplication:",a*b);
+if b!=0:
+ print("division:",a/b);
+else:
+ print("division:cannot be divided by zero");
